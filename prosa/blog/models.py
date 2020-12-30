7355bf166e83c917cacce64a239f962710ed6fd9
bres <geobres@gmail.com>
2020-12-30 23:50:48 +0200

    creating the articles page,the table of artciles with the fields *title*body*slug*date and paasing the data on the views.py and then to template

--- prosa/blog/models.py
+++ prosa/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Posts(models.Model):
+    title=models.CharField(max_length=150)
+    slug =models.SlugField()
+    body =models.TextField(max_length=2000)
+    date =models.DateTimeField(auto_now_add=True)
+    #add in thumbnail
+    #add author
+
+    def __str__(self):
+        return self.title
--- prosa/blog/views.py
+++ prosa/blog/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Posts
+
+
+
+
 
 # Create your views here.
 def home(request):
+
     return render(request,'blog/home.html')
 
+
+def articles_list(request):
+    articles=Posts.objects.all().order_by('date')
+    return render(request,'blog/articles.html',{'articles':articles } )
+
 def about(request):
-    return HttpResponse('<h1>Blog about</h1>')
+
+    return render(request,'blog/about.html')
+
