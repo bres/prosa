03a44c35d105c996af1f01a778027f561f2a298f
agreat dev <agreatdev@iMac-agreat.local>
2021-01-29 23:41:43 +0200

    crud process

--- prosa/blog/urls.py
+++ prosa/blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path('',views.home,name='home'),
     path('about/',views.about,name='about'),
     path('posts/',views.posts_list,name='posts_list'),
-    path('create/',views.post_create,name='create'),
+    path('create/',views.post_create,name='post_create'),
     path('<slug:post>/', views.post_single, name='post_single'),
+    path('update/<slug:slug>/', views.post_edit, name='post_edit'),
 ]
- 
--- prosa/blog/views.py
+++ prosa/blog/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect
 from .models import Posts
 from django.contrib.auth.decorators import login_required
-
+from .forms import CreatePost
 # Create your views here.
 def home(request):
 
@@ -22,4 +22,27 @@ def post_single(request,post):
 
 @login_required(login_url="/accounts/login/")
 def post_create(request):
-    return render(request,'blog/post_create.html')
+    if request.method=='POST':
+        form=CreatePost(request.POST)
+        if form.is_valid():
+            post=form.save(commit=False)
+            post.author=request.user
+            post.save()
+            return redirect('blog:posts_list')
+    else:
+        form=CreatePost()
+    return render(request,'blog/post_create.html',{'form':form })
+
+def post_edit(request, slug):
+    post = get_object_or_404(Posts,slug=slug)
+    if request.method == "POST":
+        form = CreatePost(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            #post.published_date = timezone.now()
+            post.save()
+            return redirect('blog:post_single', slug=post.slug)
+    else:
+        form = CreatePost(instance=post)
+    return render(request, 'blog/post_update.html', {'form': form,'post':post})
